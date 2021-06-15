45d02bc480a97cdbb9184ece5cc0305cf074825e
johannes <johannes.muljana@tafensw.edu.au>
2021-06-15 14:31:42 +1000

    update fetch in Cake component

--- src/components/Cakes.js
+++ src/components/Cakes.js
@@ -1,4 +1,23 @@
-export function Cakes() {
+import { useState, useEffect } from 'react'
+export function Cakes( props ) {
+    const [cakes,setCakes] = useState()
+
+    useEffect(() => {
+        if( cakes === undefined ) {
+            getData( props.url )
+        }
+    })
+
+    const getData = (dest) => {
+        fetch( dest )
+        .then( (res) => res.json() )
+        .then( (data) => { 
+            setCakes(data) 
+            console.log(data)
+        })
+        .catch((err) => console.log(err))
+    }
+    
     return(
         <div className="cakes">
             <h2>Cakes</h2>
